92fc90b93d1604c6521fd7fae7e4639437106a9c
Barbara <dagny099@gmail.com>
2023-08-21 15:22:26 -0500

    added support for parquet files

--- app.py
+++ app.py
@@ -11,9 +11,16 @@ import matplotlib.pyplot as plt
 st.set_page_config(
     page_title="Converse with Data",
     page_icon="🦉",
+    layout="wide"
 )
 
-conn = st.experimental_connection('s3', type=FilesConnection)
+
+TOGGLE = 's3' # 's3' OR 'local'
+if TOGGLE=='s3':
+    conn = st.experimental_connection('s3', type=FilesConnection)
+else:
+    conn = st.experimental_connection('local', type=FilesConnection)
+
 
 st.markdown('''
 # **Converse with Data**  
@@ -28,30 +35,48 @@ if "df" not in st.session_state:
     st.session_state.df = None
 
 if "openai_key" in st.session_state:
+    st.sidebar.header("Choose dataset")
+    st.sidebar.subheader("")
+    filetype = st.radio("1- Select type of data",('CSV', 'Parquet'),1)
+
     if st.sidebar.button('Press to load an example dataset'):
-        
-        #df = pd.read_csv('data/uci-cc-power-plant-data.csv')
-        df = conn.read("run-explorer-files/uci-cc-power-plant-data.csv", input_format="csv", ttl=600)
+        FILE_NAME = 'uci-cc-power-plant-data.csv'
+        FILE_DESC = '''
+                The dataset contains 9568 data points collected from a Combined Cycle Power Plant over 6 years (2006-2011), 
+                when the power plant was set to work with full load. \n
+                Features consist of the following *hourly average ambient variables* to 
+                predict the **net hourly electrical energy output (PE)** of the plant.
+                - Temperature (T),  
+                - Ambient Pressure (AP),  
+                - Relative Humidity (RH)  and 
+                - Exhaust Vacuum (V) to 
+                [Visit UCI page](https://archive.ics.uci.edu/dataset/294/combined+cycle+power+plant)
+                '''
+        if filetype=='CSV':
+            if TOGGLE=='s3':
+                df = conn.read(f"run-explorer-files/{FILE_NAME}", input_format="csv", ttl=600)
+            else:
+                df = conn.read((f'data/{FILE_NAME}'), input_format="csv", ttl=600)
+
+        elif filetype=='Parquet': 
+            FILE_NAME = 'items_w_agg_scores.parquet'
+            FILE_DESC = f'''
+            This data comes from **{TOGGLE.upper()}** parquet file
+            '''
+            if TOGGLE=='s3':
+                df = conn.read(f"run-explorer-files/{FILE_NAME}", input_format="parquet", ttl=600)
+            else:
+                #df = pd.read_csv(f'data/{FILE_NAME}')
+                df = conn.read((f'data/{FILE_NAME}'), input_format="parquet", ttl=600)
 
         st.session_state.df = df
-        st.sidebar.markdown('''
-        The dataset contains 9568 data points collected from a Combined Cycle Power Plant over 6 years (2006-2011), 
-        when the power plant was set to work with full load. \n
-        Features consist of the following *hourly average ambient variables* to 
-        predict the **net hourly electrical energy output (PE)** of the plant.
-        - Temperature (T),  
-        - Ambient Pressure (AP),  
-        - Relative Humidity (RH)  and 
-        - Exhaust Vacuum (V) to 
+        st.sidebar.markdown(FILE_DESC)
         
-
-        [Visit UCI page](https://archive.ics.uci.edu/dataset/294/combined+cycle+power+plant)
-                    
-        ''')
     st.sidebar.subheader('OR')
     uploaded_file = st.sidebar.file_uploader("",
         type="csv",
         help="Data should be in long format, one datapoint per row.",
+        label_visibility='collapsed'
     )
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
